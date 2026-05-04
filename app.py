dd155538257bd3bf7cfdf7edc146836abf755c2f
Vanitha2809 <vanithavanitha6763@gmail.com>
2026-05-04 12:21:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ db_user = os.getenv("DB_USER")
 db_pass = os.getenv("DB_PASS")
 
 print("Database User:", db_user)
-print("Database Password:", db_pass)
+
+# NEVER print password in real projects (important for security)
+print("Database Password: ******")
+
 
